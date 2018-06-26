9d1f38a9941d0eedab4a43eeb73596a93b2cc727
Shravani Khatri <khatrishravani@gmail.com>
2018-06-27 01:07:53 +0530

    Code without userdefaults

---
+++ Todoey/Controller/TodoListViewController.swift
@@ -0,0 +1,118 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Shravani on 24/06/18.
+//  Copyright © 2018 Shravani. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+        var itemArray = [Item]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let newItem1 = Item()
+        newItem1.title = "One"
+        itemArray.append(newItem1)
+        
+        let newItem2 = Item()
+        newItem2.title = "Two"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Three"
+        itemArray.append(newItem3)
+
+    }
+    
+    //MARK - Tableview Datasource method
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        //intsead of using itemArray[indexPath.row] everytime we are substituting it with 'item' variable
+        let item = itemArray[indexPath.row]
+        
+        // sets the items in array in the text property of cell
+        cell.textLabel?.text = item.title
+        
+        //setting accesoryType as checked if done property is true and viceversa using ternary operator
+        //value = condition ? valueIfTrue : valueIfFalse
+        cell.accessoryType = item.done == true ? .checkmark : .none
+        return cell
+    }
+    
+    //MARK - Tableview Delegate method
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // to remove the grey selection that appears after selection the cell
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+       /* if itemArray[indexPath.row].done == false {
+            itemArray[indexPath.row].done = true
+        }
+        else{
+            itemArray[indexPath.row].done = false
+        }*/
+        
+        tableView.reloadData()
+    }
+    
+    @IBAction func addItem(_ sender: Any) {
+        
+        var textField = UITextField()
+        // created a new alert
+        let alert = UIAlertController.init(title: "Add new item", message: " ", preferredStyle: .alert )
+        // added alert action
+        let alertAction = UIAlertAction.init(title: "Add", style: .default) { (action) in
+            
+            // Creating a new object of Item class
+            let newItem = Item()
+            
+            // assigning the texfield's value to the newItem object's title property
+            newItem.title = textField.text!
+            
+            //apending the newItem object to the array
+            self.itemArray.append(newItem)
+            
+            //reloading the table view
+            self.tableView.reloadData()
+            
+        }
+        let cancelAction = UIAlertAction.init(title: "Cancel", style: .cancel) { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        }
+        alert.addAction(cancelAction)
+        
+        // calling the add new item alertaction
+        alert.addAction(alertAction)
+        
+        
+        // added new textfield on the alert
+        alert.addTextField { (alertTextField) in
+            
+            // setting placeholder for the textfiled
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+            
+        }
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+}
+
